3f35c1a16fa03a70271d21445982ce8cfef88e4d
AA MAMUN <105873680+aamamun434@users.noreply.github.com>
2023-05-25 14:28:43 +0800

    Add files via upload
    https://codeforces.com/problemset/problem/4/A

---
+++ waterlemon.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class waterlemon {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if(a%2==0 && a>2 )
+        System.out.println("Yes");
+        else
+        System.out.println("No");
+    }
+}
